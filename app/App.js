6106c3797604e280ade0b07cd14101775e47fcf7
Nathalia Lourencao <cl1053@micronati.fiap.com.br>
2017-12-01 15:44:00 -0200

    Projeto teste finalizado

---
+++ app/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import GitHub from './components/GitHub';
+
+ReactDom.render(<GitHub />, document.getElementById('app'));
---
+++ app/components/SearchUser.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import GitHubUser from '../services/GitHubUser';
+
+class SearchUser extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+
+        GitHubUser.getByUsername(this.refs.username.value).then((response) =>{
+            this.props.updateUser(response.data);
+        });
+
+        GitHubUser.getReposByUsername(this.refs.username.value).then((response) => {
+            this.props.updateRepos(response.data);
+        });
+    }
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label>username</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        ref="username"
+                    />
+                    <button type="submit">Buscar</button>
+                </div>
+            </form>
+        );
+    }
+}
+
+// SearchUser.propTypes = {
+//     updateUser: React.PropTypes.func.isRequired,
+//     updateRepos: React.PropTypes.func.isRequired,
+// };
+
+export default SearchUser;
